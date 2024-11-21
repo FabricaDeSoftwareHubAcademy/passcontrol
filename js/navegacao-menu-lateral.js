3d0dcd4a106349c40a5f212d146ed654e800a441
Alvin <gabriel.alvin7@gmail.com>
2024-11-21 02:52:01 -0400

    Tentativa infrutífera de melhorar a responsividade

--- js/navegacao-menu-lateral.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $('.botao-menu-mobile').on('click', function(){
-        $('.').toggleClass('menu-mobile_aberto');
-        $(".area-lateral-navegacao").toggleClass('menu-mobile_fechado');
-    })
-});
-
-// alert("teste");
