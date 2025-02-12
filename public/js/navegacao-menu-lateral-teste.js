e589d2b1e0947b76f5a928c7d631fed0321d8273
GabrAlvSM <gabriel.alvin7@gmail.com>
2025-02-12 11:24:28 -0400

    Quase finalizado

--- public/js/navegacao-menu-lateral-teste.js
+++ public/js/navegacao-menu-lateral-teste.js
@@ -1,15 +1,26 @@
-// -- EM TESTE --
+//-- EM TESTE --
 
 // $(document).ready(function(){
 //     $('.botao-menu-mobile').on('click', function(){
-//         $('.area-lateral-navegacao').toggleClass('aberto');
-//         $(".area-lateral-navegacao").toggleClass('fechado');
+//         $('.background-m-mobile').toggleClass('aberto');
 //     })
 // })
 
 // $(document).ready(function(){
-//     $('.mobile-btn').on('click', function() {
-//         $('.mobile-menu').toggleClass('active');
-//         $('.mobile-btn').find('i').toggleClass('fa-x')
+//     $('.botao-menu-mobile').on('click', function() {
+//         $('.background-m-mobile').toggleClass('active');
+//         $('.botao-menu-mobile').find('i').toggleClass('fa-x')
 //     })
 // })
+
+const abrirMenuLateral = document.querySelector(".abrirMenuLateral");
+const menuLateralMobile = document.querySelector(".background-m-mobile");
+const fecharMenuLateral = document.querySelector(".areatransp");
+
+abrirMenuLateral.addEventListener("click", () =>{
+    menuLateralMobile.classList.add("menu-visivel");
+})
+
+fecharMenuLateral.addEventListener("click", () =>{
+    menuLateralMobile.classList.remove("menu-visivel");
+})
