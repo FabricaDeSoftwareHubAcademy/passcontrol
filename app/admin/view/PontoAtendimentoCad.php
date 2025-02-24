04790a1f7652e35ff12a61afca3abacc5373f7ab
Alvin <gabriel.alvin7@gmail.com>
2025-02-24 18:06:12 -0400

    Remocao de comentario irrelevante ao projeto

--- app/admin/view/PontoAtendimentoCad.php
+++ app/admin/view/PontoAtendimentoCad.php
@@ -4,9 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
-    <!-- TROCA TÍTULO QUANDO FOR UTILIZAR -->
     <title>PassControl</title> 
-    
 
     <!-- IMPORT DA FONTE -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
