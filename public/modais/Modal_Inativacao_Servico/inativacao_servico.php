82eb53fdf5ec407e95702e6bcabe1ff25cd2806b
Alvin <gabriel.alvin7@gmail.com>
2025-03-27 22:37:44 -0400

    Modificacao dos modais

---
+++ public/modais/Modal_Inativacao_Servico/inativacao_servico.php
@@ -0,0 +1,15 @@
+<div class="fundo-inativar-servico">
+    <section class="modal-inativar-servico">
+        <img src="../../../public/img/img-modais/Logo Nota Controlnt.png" alt="Logo Nota Control" class="logo">
+        <h1 class="titulo">Inativação</h1>
+
+        <hr class="linha">
+
+        <p class="texto">Deseja Inativar Esse Serviço?</p>
+        <div class="button-group">
+            <button class="botao-modal cancel">Não</button>
+            <button class="botao-modal save">Sim</button>
+        </div>
+    </section>
+</div>
+<script src="./inativacao_servico.js" defer></script>
