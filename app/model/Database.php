8332fbe506a35e3473053a1a43cc02f4421e449d
Darknes666 <gabriel49930366@gmail.com>
2025-04-11 01:10:44 -0300

    nada-mudou

--- app/model/Database.php
+++ app/model/Database.php
@@ -4,11 +4,17 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 class Database {
+    // private $conn;
+    // private string $local = '192.168.22.9';
+    // private string $db = 'passcontrol';
+    // private string $user = 'fabrica32';
+    // private string $password = 'fabrica2025';
+
     private $conn;
-    private string $local = '192.168.22.9';
+    private string $local = 'localhost';
     private string $db = 'passcontrol';
-    private string $user = 'fabrica32';
-    private string $password = 'fabrica2025';
+    private string $user = 'root';
+    private string $password = '';
 
     public function __construct() {
         $this->conecta();
