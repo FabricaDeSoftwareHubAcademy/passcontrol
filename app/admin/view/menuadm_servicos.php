312e7efb3b90bb85160af2759226aeb5e80bc29a
Darknes666 <gabriel49930366@gmail.com>
2025-04-15 11:22:59 -0400

    Teste do teste

--- app/admin/view/menuadm_servicos.php
+++ app/admin/view/menuadm_servicos.php
@@ -25,6 +25,9 @@
     <script src="../../../public/js/navegacao-menu-lateral.js" defer></script>
     <script src="../../../public/js/monitor-modal.js" defer></script>
     <!-- <script src="../../../public/js/modal_cadastro_guiche_adm.js" defer></script> -->
+    <script src="../../../public/modais/ModalCadastrodosServicos/cadastro_servicos.js" defer></script>
+
+
 
     <link rel="shortcut icon" type="imagex/png" href="../../../public/img/Logo-Nota-Controlnt.ico">
 </head>
