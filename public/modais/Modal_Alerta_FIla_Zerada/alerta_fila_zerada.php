a6aad1ddeb696f96efd584000d03ff95faefe2d3
Juliana <juliana29.bmartins@gmail.com>
2025-05-28 11:48:18 -0400

    padronizaçãodos modais pt1

--- public/modais/Modal_Alerta_FIla_Zerada/alerta_fila_zerada.php
+++ public/modais/Modal_Alerta_FIla_Zerada/alerta_fila_zerada.php
@@ -3,7 +3,7 @@
         <img src="../../../public/img/img-modais/Logo Nota Controlnt.png" alt="Logo Nota Control" class="logo-alertar-fila">
         <h1 class="modal-title-alertar-fila">Alerta!</h1>
         <hr class="modal-divider-alertar-fila">
-        <p class="modal-message-alertar-fila"><b>No Momento a Fila Está Vazia, Aguarde Novas Senhas!</b></p>
+        <p class="modal-message-alertar-fila"><b>No Momento a Fila Está Vazia. Aguarde Novas Senhas!</b></p>
         <div class="button-group-alertar-fila">
             <button class="botao-modal-alertar voltar_AlertaFilaZerada">Voltar</button>
         </div>
--- public/modais/Modal_Alterar_Status/alterar_status_usuario.php
+++ public/modais/Modal_Alterar_Status/alterar_status_usuario.php
@@ -3,7 +3,7 @@
             <img src="../../../public/img/img-modais/Logo Nota Controlnt.png" alt="Logo Nota Control" class="logo_AltStatusUsu">
             <h1 class="titulo_AltStatusUsu">Confirmação</h1>
             <hr class="linha_AltStatusUsu">
-            <p class="texto_AltStatusUsu"><b>Deseja Alterar o Status Deste Usuário?</b></p>
+            <p class="texto_AltStatusUsu"><b>Deseja Alterar o Status?</b></p>
             <div class="button-group_AltStatusUsu">
                 <button type="button" class="botao-modal_AltStatusUsu cancel-atv-inatv">Não</button>
                 <button type="button" class="botao-modal_AltStatusUsu save-atv-inatv">Sim</button>
