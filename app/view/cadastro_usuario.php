9311cf3eead9dfa6e23837846d5dcb08d2179f55
joaoguilhermes2 <142962028+joaoguilhermes2@users.noreply.github.com>
2025-06-02 08:08:06 -0400

    Atualização Nomes JS e caminhos

--- app/view/cadastro_usuario.php
+++ app/view/cadastro_usuario.php
@@ -27,7 +27,7 @@
     <!-- JS -->
     <script src="../../public/js/navegacao-menu-lateral.js" defer></script>
     <script src="../../public/js/monitor-modal.js" defer></script>
-    <script src="../../public/js/checkbox-seleciona-todos.js" defer></script>
+    <script src="../../public/js/checkbox_seleciona_todos.js" defer></script>
     <script src="../js/usuario_cadastrar.js" defer></script>
     <script src="../js/validar_cpf.js" defer></script>
 
