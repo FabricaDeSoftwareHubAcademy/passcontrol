cb8809ec64917ed3b706d8024263a905c6cd2502
gersin231 <gersonsouza4839@gmail.com>
2025-06-03 09:39:48 -0400

    Update modal_confirmacao_senha.js

--- public/js/modal_confirmacao_senha.js
+++ public/js/modal_confirmacao_senha.js
@@ -3,16 +3,13 @@
     const modalContainer_ConfSenha = document.querySelector(".fundo-container-confirmacao-senha");
     const buttonFechar_ConfSenha = document.querySelector(".Okay_ConfSenha");
 
-    // Abre o modal ao clicar em "Entrar"
     buttonAbrir_ConfSenha.addEventListener("click", () => {
         modalContainer_ConfSenha.classList.add("show");
     });
 
-    // Fecha o modal e redireciona ao clicar em "Ok"
     buttonFechar_ConfSenha.addEventListener("click", () => {
         modalContainer_ConfSenha.classList.remove("show");
 
-        // Redirecionamento após clicar "Ok"
         window.location.href = "../../app/view/recuperar_senha_codigo.php"; // substitua pelo caminho real
     });
 
