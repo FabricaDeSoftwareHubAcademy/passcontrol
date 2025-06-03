b8f948889eb932cab019c9060025598f632b4b5e
GraziCarassini <duegra@gmail.com>
2025-06-03 10:43:45 -0400

    instalacao vendor e teste primeiro acesso

---
+++ testeprimeiroacesso/modal_primeiroacesso_alterarsenha.js
@@ -0,0 +1,31 @@
+
+buttonSalvar_AltSenha.addEventListener("click", () => {
+    const senhaAtual = document.querySelector(".input-senha-atual").value;
+    const novaSenha = document.querySelector(".input-nova-senha").value;
+    const confirmaSenha = document.querySelector(".input-conf-nova-senha").value;
+
+    fetch("trocar_senha.php", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: new URLSearchParams({
+            senha_atual: senhaAtual,
+            nova_senha: novaSenha,
+            confirma_senha: confirmaSenha
+        })
+    })
+    .then(res => res.json())
+    .then(data => {
+        if (data.sucesso) {
+            alert(data.sucesso);
+            window.location.href = "./app/view/atendimento.php";
+        } else {
+            alert(data.erro);
+        }
+    })
+    .catch(err => {
+        console.error("Erro:", err);
+        alert("Erro inesperado.");
+    });
+});
