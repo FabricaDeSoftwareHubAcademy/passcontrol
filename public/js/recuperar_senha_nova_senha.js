243441ec13eda0ea88322f53c5c1d907d6928184
Fred Lopes <fredlopes.priv@outlook.com>
2025-06-05 10:52:26 -0400

    feat: refatoracao pagina digitar nova senha

--- public/js/recuperar_senha_nova_senha.js
+++ public/js/recuperar_senha_nova_senha.js
@@ -1,46 +1,46 @@
-function validarSenha(novaSenha) {
+function validarSenha(nova_senha) {
     const liDigito = document.getElementById('digito');
-    const liMaiuscula = document.getElementById('maiusca');
+    const liMaiuscula = document.getElementById('maiuscula');
     const liNumero = document.getElementById('numero');
-    const liCaracEspec = document.getElementById('caracEspec');
+    const liCaractereEspecial = document.getElementById('caractere_especial');
 
-    if (novaSenha.length >= 8) {
-        liDigito.classList.add('requisitosValidos');
+    if (nova_senha.length >= 8) {
+        liDigito.classList.add('requisitos_validos');
     } else {
-        liDigito.classList.remove('requisitosValidos');
-        liDigito.classList.add('requisitosInvalidos');
+        liDigito.classList.remove('requisitos_validos');
+        liDigito.classList.add('requisitos_invalidos');
     }
 
-    if (/[A-Z]/.test(novaSenha)) {
-        liMaiuscula.classList.add('requisitosValidos');
+    if (/[A-Z]/.test(nova_senha)) {
+        liMaiuscula.classList.add('requisitos_validos');
     } else {
-        liMaiuscula.classList.remove('requisitosValidos');
-        liMaiuscula.classList.add('requisitosInvalidos');
+        liMaiuscula.classList.remove('requisitos_validos');
+        liMaiuscula.classList.add('requisitos_invalidos');
     }
 
-    if (/[0-9]/.test(novaSenha)) {
-        liNumero.classList.add('requisitosValidos');
+    if (/[0-9]/.test(nova_senha)) {
+        liNumero.classList.add('requisitos_validos');
     } else {
-        liNumero.classList.remove('requisitosValidos');
-        liNumero.classList.add('requisitosInvalidos');
+        liNumero.classList.remove('requisitos_validos');
+        liNumero.classList.add('requisitos_invalidos');
     }
 
-    if (/[!@#$%^&*(),.?":{}|<>]/.test(novaSenha)) {
-        liCaracEspec.classList.add('requisitosValidos');
+    if (/[!@#$%^&*(),.?":{}|<>]/.test(nova_senha)) {
+        liCaractereEspecial.classList.add('requisitos_validos');
     } else {
-        liCaracEspec.classList.remove('requisitosValidos');
-        liCaracEspec.classList.add('requisitosInvalidos');
+        liCaractereEspecial.classList.remove('requisitos_validos');
+        liCaractereEspecial.classList.add('requisitos_invalidos');
     }
 }
 
-document.getElementById('novaSenha').addEventListener('input', function() {
+document.getElementById('nova_senha').addEventListener('input', function() {
     validarSenha(this.value);
 });
 
 
 
-const togglePassword = document.querySelector("#togglePassword");
-const password = document.querySelector("#confirmSenha");
+const togglePassword = document.querySelector("#toggle_password");
+const password = document.querySelector("#confirmar_senha");
 
 togglePassword.addEventListener("click", function(){
     const type = password.type === "password" ? "text" : "password";
@@ -53,8 +53,8 @@ togglePassword.addEventListener("click", function(){
 
 
 
-const togglePasswordNv = document.querySelector("#togglePasswordNv");
-const passwordNv = document.querySelector("#novaSenha");
+const togglePasswordNv = document.querySelector("#toggle_password_novo");
+const passwordNv = document.querySelector("#nova_senha");
 
 togglePasswordNv.addEventListener("click", function(){
     const type = passwordNv.type === "password" ? "text" : "password";
@@ -67,9 +67,9 @@ togglePasswordNv.addEventListener("click", function(){
 
 
 
-function validarConfSenha() {
-    const senha = document.getElementById("novaSenha").value.trim();
-    const confSenha = document.getElementById("confirmSenha").value.trim();
+function validarConfirmacaoSenha() {
+    const senha = document.getElementById("nova_senha").value.trim();
+    const confSenha = document.getElementById("confirmar_senha").value.trim();
 
     if (senha === "" || confSenha === "") {
         alert("Preencha todos os campos");
