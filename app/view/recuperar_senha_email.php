5aa0e351159e7de123e09d4c71ba380939a0cd27
Fred Lopes <fredlopes.priv@outlook.com>
2025-06-06 11:30:32 -0400

    feat: fluxo de login 100% refatorado

--- app/view/recuperar_senha_email.php
+++ app/view/recuperar_senha_email.php
@@ -5,34 +5,39 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Passcontrol</title>
  
-    <link rel="stylesheet" href="../../public/css/recuperar_senha_email.css">
+    <link rel="stylesheet" href="../../public/css/root.css">
+    <link rel="stylesheet" href="../../public/css/forms.css">
     <link rel="stylesheet" href="../../public/css/modal_confirmacao_senha.css">
+
     <link rel="shortcut icon" href="../../public/img/favicon.ico" type="image/x-icon">
+
+    <script src="../../public/js/modal_confirmacao_envio_email.js" defer></script>
 </head>
 <body class="body_login background_image">
     <main class="container_login">
-        <div class="container_animation">
-            <img src="../../public/img/logo_png/animation_logo_top.png" alt="imagem da animação topo" class="image_animation image_animation_top">
-            <img src="../../public/img/logo_png/animation_logo_mid.png" alt="imagem da animação meio" class="image_animation image_animation_mid">
-            <img src="../../public/img/logo_png/animation_logo_bot.png" alt="imagem da animação baixo" class="image_animation image_animation_bot">
+        <div class="card_left_login">
+            <div class="container_animation">
+                <img src="../../public/img/logo_animation/animation_logo_top.png" alt="imagem da animação topo" class="image_animation image_animation_top">
+                <img src="../../public/img/logo_animation/animation_logo_mid.png" alt="imagem da animação meio" class="image_animation image_animation_mid">
+                <img src="../../public/img/logo_animation/animation_logo_bot.png" alt="imagem da animação baixo" class="image_animation image_animation_bot">
+            </div>
+            <h1 class="title">PASS CONTROL</h1>
         </div>
-        <h1 class="title">PASS CONTROL</h1>
  
-        <form action="" class="form form_login">
+        <form action="" class="form">
             <div class="input_group">
                 <label class="label_recuperar_senha" for="">Recuperar Senha</label>
                 <input class="input" type="email" name="" id="" placeholder="E-mail">
             </div>
-            <p class="text_recuperar_senha">Você receberá um código de segurança no e-mail informado para validar sua nova senha.</p>
-            <div class="container_button">
-            <button type="button" class="button open-confirmacao-senha">Entrar</button>
 
+            <p class="text_recuperar_senha">Você receberá um código de segurança no e-mail informado para validar sua nova senha.</p>
+
+            <div class="container_button">
+                <button type="button" class="button open-confirmacao-senha">Entrar</button>
             </div>
         </form>
     </main>
  
     <?php include '../../public/modais/modal_confirmacao_envio_email.php'; ?>
-    <script src="../../public/js/modal_confirmacao_envio_email.js"></script>
 </body>
 </html>
- 
--- index.php
+++ index.php
@@ -1,83 +1,56 @@
 <?php
-// index.php
-session_start();
+    session_start();
 ?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PassControl</title>
 
-    <!-- CSS -->
-    <link rel="stylesheet" href="./public/css/login.css">
+    <link rel="stylesheet" href="./public/css/root.css">
+    <link rel="stylesheet" href="./public/css/forms.css">
 
-    <!-- LOGO / ÍCONES -->
     <link rel="shortcut icon" href="./public/img/favicon.ico" type="image/x-icon">
-    <link rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
-          integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
-          crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-    <!-- JS -->
     <script src="./app/js/mascara_cpf.js" defer></script>
     <script src="./public/js/login.js" defer></script>
 </head>
-
 <body class="body_login background_image">
-
     <main class="container_login">
         <div class="card_left_login">
             <div class="container_animation">
-                <img src="./public/img/logo_png/animation_logo_top.png" alt="imagem da animação topo"
-                     class="image_animation image_animation_top">
-                <img src="./public/img/logo_png/animation_logo_mid.png" alt="imagem da animação meio"
-                     class="image_animation image_animation_mid">
-                <img src="./public/img/logo_png/animation_logo_bot.png" alt="imagem da animação baixo"
-                     class="image_animation image_animation_bot">
+                <img src="./public/img/logo_animation/animation_logo_top.png" alt="imagem da animação topo" class="image_animation image_animation_top">
+                <img src="./public/img/logo_animation/animation_logo_mid.png" alt="imagem da animação meio" class="image_animation image_animation_mid">
+                <img src="./public/img/logo_animation/animation_logo_bot.png" alt="imagem da animação baixo" class="image_animation image_animation_bot">
             </div>
             <h1 class="title">PASS CONTROL</h1>
         </div>
 
         <div class="container_lines">
             <div class="line_top"></div>
-
             <div class="container_arrow">
-                <img src="./public/img/logo_png/arrow.png" alt="setas">
-                <img src="./public/img/logo_png/arrow.png" alt="setas">
-                <img src="./public/img/logo_png/arrow.png" alt="setas">
+                <img src="./public/img/arrow.png" alt="setas">
+                <img src="./public/img/arrow.png" alt="setas">
+                <img src="./public/img/arrow.png" alt="setas">
             </div>
-
             <div class="line_bot"></div>
         </div>
 
         <div class="card_right_login">
             <h1 class="title">Olá, Seja Bem-Vindo!</h1>
-
-            <!--
-                O formulário agora envia para app/actions/usuario_logar.php.
-                Observe que o campo de senha deve ter name="senha",
-                para coincidir com o que o login.php espera e não password.
-            -->
-            <form id="form_login" method="POST" class="form form_login">
+            <form action="./app/actions/usuario_logar.php" method="post" class="form form_login">
                 <div class="input_group input_group_login">
                     <label for="cpf" class="label">CPF</label>
-                    <input type="text" name="cpf" id="cpf" class="input" placeholder="Digite seu CPF">
-
+                    <input type="text" name="cpf" id="cpf" class="input" placeholder="000.000.000-00">
                 </div>
 
                 <div class="input_group input_group_login">
                     <label for="senha" class="label">Senha</label>
                     <div class="password_group">
-                        <input
-                            type="password"
-                            name="senha"
-                            id="input_password"
-                            class="input input_password"
-                            placeholder="••••••••"
-                            required
-                        >
+                        <input type="password" name="senha" id="input_password" class="input input_password" placeholder="••••••••" required>
                         <i id="toggle_password" class="fas fa-eye toggle_password"></i>
                     </div>
                 </div>
@@ -87,20 +60,17 @@ session_start();
                 </div>
 
                 <div class="container_button">
-                    <button id="btn_login" type="submit" class="button">Entrar</button>
+                    <button type="submit" class="button">Entrar</button>
                 </div>
             </form>
         </div>
     </main>
 
     <?php
-    // Verifica se precisa exibir o modal de primeiro acesso
-    if (isset($_SESSION['primeiro_login']) && $_SESSION['primeiro_login']) {
-        // Inclui o modal de alteração de senha (apenas uma vez)
-        include './app/view/modais/modal_primeiroacesso_alterarsenha.php';
-        unset($_SESSION['primeiro_login']);
-    }
+        if (isset($_SESSION['primeiro_login']) && $_SESSION['primeiro_login']) {
+            include './app/view/modais/modal_primeiroacesso_alterarsenha.php';
+            unset($_SESSION['primeiro_login']);
+        }
     ?>
-
 </body>
 </html>
