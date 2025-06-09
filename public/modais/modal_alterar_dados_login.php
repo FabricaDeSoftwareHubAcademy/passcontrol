fcb805c9b7f64d58b78c4d631371905a8cb92acd
joaoguilhermes2 <142962028+joaoguilhermes2@users.noreply.github.com>
2025-06-09 09:20:15 -0400

    Criando a funcionalidade de alteração dos dados pessoais e senha

---
+++ public/modais/modal_alterar_dados_login.php
@@ -0,0 +1,10 @@
+<div class="edit_dados">
+    <section class="edit-dados-login">
+        <img src="../../public/img/img-modais/Logo Nota Controlnt.png" alt="Loading...">
+        <h1 class="title">Alterar Dados</h1>
+        <hr class="line">
+        <form class="editDados" id="formEditDados" method="POST" enctype="multipart/form-data">
+            
+        </form>
+    </section>
+</div>
