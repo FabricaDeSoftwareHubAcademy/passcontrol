518297be1f925d27c9df7e6c9c6f864928aad8f2
LuanRech2205 <curso.luan22@gmail.com>
2025-06-09 11:24:22 -0400

    no trabalho de fazer o fluxo do modal editar

--- app/view/servicos.php
+++ app/view/servicos.php
@@ -3,48 +3,11 @@ require '../classes/Servico.php';
 
 $novo_servico = new Servico();
 $servicos_cadastrados = $novo_servico->buscar();
-?>
 
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PassControl</title>
-
-    <!-- IMPORT DA FONTE -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
-
-    <!-- IMPORT DO CSS -->
-    <link rel="stylesheet" href="../../public/css/navegacao.css">
-    <link rel="stylesheet" href="../../public/Modal_Alterar_Dados_Pessoais/alterar_dados_pessoais.css">
-    <link rel="stylesheet" href="../../public/Modal_Alterar_Senha/alterar_senha.css">
-    
-    <link rel="stylesheet" href="../../public/css/servico.css">
-    <link rel="stylesheet" href="../../public/css/edicao_servico.css">
-    <link rel="stylesheet" href="../../public/css/modal_cadastro_servico.css">
-    <link rel="stylesheet" href="../../public/css/modal_confirmacao_dados_servico.css">
-    <link rel="stylesheet" href="../../public/css/modal_status_servico.css">
-    <link rel="stylesheet" href="../../public/css/modal_confirmacao_servico.css">
-
-    <!-- JS -->
-    <script src="../../public/js/navegacao_menu_lateral.js" defer></script>
-    <script src="../../public/js/monitor_modal.js" defer></script>
-    <script src="../../public/js/modal_cadastro_confirmacao_servico.js" defer></script>
-    <script src="../../public/js/modal_edicao_confirmar_servico.js" defer></script>
-    <script src="../../public/js/modal_status_confirmacao_servico.js" defer></script>
-
-
-    <link rel="shortcut icon" type="imagex/png" href="../../../public/img/Logo-Nota-Controlnt.ico">
-</head>
-<body class="control-body-navegacao">
-    <?php
     include "./navegacao.php";
     include "../actions/servico_listar.php";
     include "../actions/servico_editar.php";
-    ?>
+?>
 
 <section class="Area-Util-Projeto">
     <!-- INICIO DA ÁREA ÚTIL DA PÁGINA -->
@@ -77,13 +40,13 @@ $servicos_cadastrados = $novo_servico->buscar();
                                         <td class="indentificador-menor">'.$servico->codigo_servico.'</td>
                                         <td class="editar-menor">
                                             <div class="editar">
-                                                <button class="bot-editar" id="chamamodal" id_value ='.$servico->id_servico.'>
+                                                <button class="bot-editar chamamodal" id="" id_value ='.$servico->id_servico.' data-btn-modal="editar-servico">
                                                     <img id="icone-editar" src="../../public/img/icons/editar.png" alt="Editar">
                                                 </button>
                                             </div>
                                         </td>
                                         <td class="inativar-menor">
-                                            <button id="switch_status" id_value_switch="'.$servico->id_servico.'"  class="toggle-btn '.$estadoAtivo.'">
+                                            <button id="" id_value_switch="'.$servico->id_servico.'"  class="switch_status toggle-btn '.$estadoAtivo.'" data-btn-modal="status-servico">
                                                 <div class="circulo"></div>
                                             </button>
                                         </td>
@@ -98,15 +61,16 @@ $servicos_cadastrados = $novo_servico->buscar();
         <div class="botoesVoltar-Cadastrar">
             <button type="button" class="botao-voltar" onclick="window.location.href='menuadm_servicos.php';">Voltar</button>
             <!-- <button type="button" id="abrirModal" class="botao-cadastro">Cadastrar</button> -->
-            <button type="button" id="btn_cadastrar_servico" class="botao-cadastro">Cadastrar</button>
+            <button type="button" id="btn_cadastrar_servico" class="botao-cadastro" data-btn-modal="cadastrar">Cadastrar</button>
 
         <?php
-        include "./monitor_modal.php";
-        include "../../public/modais/modal_cadastro_servico.php";
-        include "../../public/modais/modal_confirmacao_cadastro_servico.php";
-        include "../../public/modais/modal_confirmacao_dados_servicos.php";
-        include "../../public/modais/modal_confirmacao_edicao_servico.php";
-        include "../../public/modais/modal_satus_servico.php";
+            include_once "./monitor_modal.php";
+            include_once "../../public/modais/modal_cadastro_servico.php";
+            include_once "../../public/modais/modal_confirmacao_dados_servicos.php";
+            include_once "../../public/modais/modal_edicao_servico.php";
+            include_once "../../public/modais/modal_confirmacao_edicao_servico.php";
+            include_once "../../public/modais/modal_satus_servico.php";
+            include_once "../../public/modais/modal_sucesso.php";
         ?>
 </section>
 
