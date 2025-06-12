496f4ef0bae8771d24afe7285c3bc7d3a97781bc
LuanRech2205 <curso.luan22@gmail.com>
2025-06-12 11:14:43 -0400

    fluxo do modal no status servico finalizado, terminado backend do status do servico

--- app/view/servicos.php
+++ app/view/servicos.php
@@ -19,46 +19,44 @@ $servicos_cadastrados = $novo_servico->buscar();
             <div class="sev"><p id="Ponto-atendimento">Serviços</p></div>
             <div class="linha-divisoria-Ponto-atendimento"></div>
         </div>
-        <div id="tela-branca-Ponto-atendimento"> 
+        <div id="tela-branca-Ponto-atendimento">
             <div class="tabela-responsiva-Ponto-atendimento">
                 <table id="table table-striped" class="tabela-Ponto-atendimento">
                     <thead class="cabecaTabelaPonto-atendimento">
                         <tr class="topo-tabela-servicos">
                             <th  class="cabecalho-tabela1">Código</th>
-                            <th class="indentificador-menor"  class="cabecalho-tabela2">Serviço</th>
+                            <th class="indentificador-menor"  class="cabecalho-tabela2">Nome do Serviço</th>
                             <th class="editar-menor"  class="cabecalho-tabela3">Editar</th>
                             <th class="inativar-menor"  class="cabecalho-tabela1">Status</th>
                         </tr>
                     </thead>
                     <tbody class="resto-tabela-Ponto-atendimento">
-                            <?php
-                                foreach($servicos_cadastrados as $servico) {
-                                    $estadoAtivo = ($servico->status_servico == 'ATIVO') ? 'active' : '';
-                                    echo '
-                                    <tr>
-                                        <td>'.$servico->nome_servico.'</td>
-                                        <td class="indentificador-menor">'.$servico->codigo_servico.'</td>
-                                        <td class="editar-menor">
-                                            <div class="editar">
-                                                <button class="bot-editar chamamodal" id="" id_value ='.$servico->id_servico.' data-btn-modal="editar-servico">
-                                                    <img id="icone-editar" src="../../public/img/icons/editar.png" alt="Editar">
-                                                </button>
-                                            </div>
-                                        </td>
-                                        <td class="inativar-menor">
-                                            <button id="" id_value_switch="1"  class="switch_status toggle-btn '.$estadoAtivo.'" data-btn-modal="status-servico">
-                                        
-                                                <div class="circulo"></div>
-                                            </button>
-                                        </td>
-    
-                                    </tr>';
-                                }
-                            ?>
-                        </tbody>
+                        <?php foreach($servicos_cadastrados as $servico): ?>
+                            <?php $classeStatus = ($servico->status_servico == 1) ? 'active' : ''; ?>
+                            <tr>
+                                <td><?= $servico->nome_servico ?></td>
+                                <td class="indentificador-menor"><?= $servico->codigo_servico ?></td>
+                                <td class="editar-menor">
+                                    <div class="editar">
+                                        <button class="bot-editar chamamodal" id_value="<?= $servico->id_servico ?>" data-btn-modal="editar-servico">
+                                            <img id="icone-editar" src="../../public/img/icons/editar.png" alt="Editar">
+                                        </button>
+                                    </div>
+                                </td>
+                                <td class="inativar-menor">
+                                    <button class="switch_status toggle-btn <?= $classeStatus ?>" 
+                                            id_value_switch="<?= $servico->id_servico ?>" 
+                                            data-btn-modal="status-servico">
+                                        <div class="circulo"></div>
+                                    </button>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
                 </table>
             </div>
         </div>
+        <!-- </div> <button id="" id_value_switch="1"  class="switch_status toggle-btn '.$estadoAtivo.'" data-btn-modal="status-servico"></button> -->
         <div class="botoesVoltar-Cadastrar">
             <button type="button" class="botao-voltar" onclick="window.location.href='menuadm_servicos.php';">Voltar</button>
             <!-- <button type="button" id="abrirModal" class="botao-cadastro">Cadastrar</button> -->
