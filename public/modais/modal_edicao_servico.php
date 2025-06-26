a96da740a3ca84b625c93df24d1e9de5baa0db29
LuanRech2205 <curso.luan22@gmail.com>
2025-06-26 11:14:26 -0400

    nomes de classes, id, inpust, etc corrigdo para serviços para não ter mais conflito com ponto de atendimento

--- public/modais/modal_edicao_servico.php
+++ public/modais/modal_edicao_servico.php
@@ -2,35 +2,35 @@
 <!-- ************************************   EDITAR SERVIÇO  ************************************ -->
 
 <!-- Modal  EDITAR Ponto de Atendimento-->
-<div class="modal-container" data-nome-modal="editar-servico">
-    <section class="modal">
-        <img src="../img/img-modais/Logo Nota Controlnt.png" alt="Logo Nota Control" class="logo">
-        <h1 class="titulo">Editar Serviço</h1>
-        <hr class="linha-horizontal">
-        <form id="formulario_editar" method="POST" enctype="multipart/form-data">
+<div class="modal-container-servico" data-nome-modal="editar-servico">
+    <section class="modal-serv">
+        <img src="../../public/img/img-modais/Logo Nota Controlnt.png" alt="Logo Nota Control" class="logo-servico">
+        <h1 class="titulo-edit-servico">Editar Serviço</h1>
+        <hr class="linha-horizontal-servico">
+        <form id="formulario_editar_servico" method="POST" enctype="multipart/form-data">
             <!-- NOME DO SERVIÇO  -->
-            <div class="inf-modal">
-                <div class="container">
+            <div class="inf-modal-edit-servico">
+                <div class="container-edit-nomeservico">
                     <label class="label"><b>Nome do Serviço</b></label>
-                    <input type="text" id="nome_ponto_atendimento" name="nome_servico" class="input-text">
+                    <input type="text" id="nome_ponto_atendimento" name="nome_servico" class="input-text-edit">
                     <input type="hidden" id="id_ponto_atendimento" name="id_servico">
                 </div>
             </div>
             <!-- CÓDIGO SERVIÇO -->
-            <div class="servico">
-                <label class="label"><b>Código do Serviço</b></label>
-                <input type="text" id="identificador_ponto_atendimento" name="codigo_servico" class="input-text">
+            <div class="edit-codigo-servico">
+                <label class="label-serv"><b>Código do Serviço</b></label>
+                <input type="text" id="identificador_ponto_atendimento" name="codigo_servico" class="input-text-edit">
             </div>
             <!-- IMAGEM DO SERVIÇO -->
-            <div class="servico">
+            <div class="img-edit-servico">
                 <label class="label"><b>Imagem</b></label>
-                <input type="file" id="imagem_ponto_atendimento" name="url_imagem_servico" class="input-text" accept="image/*">
+                <input type="file" id="imagem_edit_servico" name="url_imagem_servico" class="input-text-edit" accept="image/*">
                 <img id="preview_imagem_editar" src="#" alt="Preview da Imagem" style="display:none; width: 80px, heigt:80px; height:80px; margin-top:10px; object-fit:cover; border-radius:8px;">
             </div>
             <!-- BOTÕES -->
-            <div class="button-grup">
-                <button class="botao-modal cancel">Voltar</button>
-                <button class="botao-modal">Salvar</button>
+            <div class="button-grup-edit-servico">
+                <button class="botao-modal-edit-serv cancelEditServ">Voltar</button>
+                <button class="botao-modal-edit-serv saveEditServ">Salvar</button>
             </div>
         </form>
     </section>
