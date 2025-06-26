a639da8453491c603575ffa016643fca922b9650
Alvin <gabriel.alvin7@gmail.com>
2025-06-26 03:12:46 -0400

    fix: ajuste no caminho dos icones dos modais utilizados no crud do usuario

--- public/modais/modal_confirmacao_dados.php
+++ public/modais/modal_confirmacao_dados.php
@@ -1,6 +1,6 @@
 <div class="fundo-container-confirmacao-dados">
     <section class="modal-confirmacao-dados">
-        <img src="../../../public/img/img-modais/Logo Nota Controlnt.png" alt="Logo Nota Control" class="logo-confirmacao-dados">
+        <img src="../../public/img/img-modais/Logo Nota Controlnt.png" alt="Logo Nota Control" class="logo-confirmacao-dados">
         <h1 class="modal-title-confirmacao-dados">Confirmação</h1>
         <hr class="modal-divider-confirmacao-dados">
         <p class="modal-message-confirmacao-dados"><b>Salvo com sucesso!</b></p>
