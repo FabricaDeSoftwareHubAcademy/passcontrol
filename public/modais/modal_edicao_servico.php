141271aa0c5022afe32a402712327a368c305a6d
LuanRech2205 <curso.luan22@gmail.com>
2025-07-03 11:29:14 -0400

    organização dos modais

--- public/modais/modal_edicao_servico.php
+++ public/modais/modal_edicao_servico.php
@@ -1,7 +1,7 @@
 
 <!-- ************************************   EDITAR SERVIÇO  ************************************ -->
 
-<!-- Modal  EDITAR Ponto de Servico-->
+<!-- Modal  EDITAR Servico-->
 <div class="modal-container-servico" data-nome-modal="editar-servico">
     <section class="modal-serv">
         <img src="../../public/img/icons/logo_control.svg" alt="Logo Nota Control" class="logo-servico">
@@ -10,20 +10,20 @@
         <form id="formulario_editar_servico" method="POST" enctype="multipart/form-data">
             <!-- NOME DO SERVIÇO  -->
             <div class="inf-modal-edit-servico">
-                <div class="container-edit-nomeservico">
-                    <label class="label"><b>Nome do Serviço</b></label>
+                <div class="container-edit-nome-servico">
+                    <label class="label-servico"><b>Nome do Serviço</b></label>
                     <input type="text" id="nome_edit_servico" name="nome_servico" class="input-text-edit">
                     <input type="hidden" id="id_edit_servico" name="id_servico">
                 </div>
             </div>
             <!-- CÓDIGO SERVIÇO -->
             <div class="edit-codigo-servico">
-                <label class="label-serv"><b>Código do Serviço</b></label>
+                <label class="label-servico"><b>Código do Serviço</b></label>
                 <input type="text" id="identificador_ponto_atendimento" name="codigo_servico" class="input-text-edit">
             </div>
             <!-- IMAGEM DO SERVIÇO -->
             <div class="img-edit-servico">
-                <label class="label"><b>Editar Imagem Serviço</b></label>
+                <label class="label-servico"><b>Editar Imagem Serviço</b></label>
                 <img id="preview_imagem_editar" src="#" alt="Sem Imagem" class="manipula-img-editar" >
                 <input type="file" id="imagem_edit_servico" name="url_imagem_servico" class="input-text-edit" accept="image/*">
             </div>
