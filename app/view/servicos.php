7e8d2344ed6d7f701f7636e37ce72dba69f1d53e
GabrAlvSM <gabriel.alvin7@gmail.com>
2025-07-04 11:09:52 -0400

    fix: correcao na pagina de servicos

--- app/view/servicos.php
+++ app/view/servicos.php
@@ -10,9 +10,7 @@
     <!-- FONTE -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
 
     <!-- CSS -->
     <link rel="stylesheet" href="../../public/css/navegacao.css">
@@ -28,7 +26,6 @@
     <link rel="stylesheet" href="../../public/css/modal_confirmacao_dados.css">
     <link rel="stylesheet" href="../../public/css/modal_aviso_erro.css">
 
-
     <!-- JS -->
     <script src="../../public/js/navegacao_menu_lateral.js" defer></script>
     <script src="../../public/js/monitor_modal.js" defer></script>
@@ -37,86 +34,87 @@
     <script src="../js/servico_editar.js" defer></script>
     <script src="../js/servico_alterar_status.js" defer></script>
 
-
     <!-- LOGO -->
     <link rel="shortcut icon" type="imagex/png" href="../../public/img/Logo-Nota-Controlnt.ico">
 </head>
-<?php
-require '../classes/Servico.php';
 
-$novo_servico = new Servico();
-$servicos_cadastrados = $novo_servico->buscar();
+<body>
+    <?php
+    require '../classes/Servico.php';
 
-include "./navegacao.php";
-include "../actions/servico_listar.php";
-?>
+    $novo_servico = new Servico();
+    $servicos_cadastrados = $novo_servico->buscar();
 
-<section class="Area-Util-Projeto">
-    <!-- INICIO DA ÁREA ÚTIL DA PÁGINA -->
-    <div id="PontoAtendimentoCad">
-        <div class="topo-tela-PontoAtendimentoCad">
-            <div class="campo-busca">
-                <input id="buscar-Ponto-atendimento" type="text" placeholder="Buscar Registro">
+    include_once "./navegacao.php";
+    include_once "../actions/servico_listar.php";
+    ?>
+
+    <section class="Area-Util-Projeto">
+        <!-- INICIO DA ÁREA ÚTIL DA PÁGINA -->
+        <div id="PontoAtendimentoCad">
+            <div class="topo-tela-PontoAtendimentoCad">
+                <div class="campo-busca">
+                    <input id="buscar-Ponto-atendimento" type="text" placeholder="Buscar Registro">
+                </div>
+                <div class="sev">
+                    <p id="Ponto-atendimento">Serviços</p>
+                </div>
+                <div class="linha-divisoria-Ponto-atendimento"></div>
             </div>
-            <div class="sev">
-                <p id="Ponto-atendimento">Serviços</p>
-            </div>
-            <div class="linha-divisoria-Ponto-atendimento"></div>
-        </div>
-        <div id="tela-branca-Ponto-atendimento">
-            <div class="tabela-responsiva-Ponto-atendimento">
-                <table id="table table-striped" class="tabela-Ponto-atendimento">
-                    <thead class="cabecaTabelaPonto-atendimento">
-                        <tr class="topo-tabela-servicos">
-                            <th class="cabecalho-tabela1">Código</th>
-                            <th class="indentificador-menor" class="cabecalho-tabela2">Nome do Serviço</th>
-                            <th class="editar-menor" class="cabecalho-tabela3">Editar</th>
-                            <th class="inativar-menor" class="cabecalho-tabela1">Status</th>
-                        </tr>
-                    </thead>
-                    <tbody class="resto-tabela-Ponto-atendimento">
-                        <?php foreach ($servicos_cadastrados as $servico): ?>
-                            <?php $classeStatus = ($servico->status_servico == 1) ? 'active' : ''; ?>
-                            <tr>
-                                <td><?= $servico->nome_servico ?></td>
-                                <td class="indentificador-menor"><?= $servico->codigo_servico ?></td>
-                                <td class="editar-menor">
-                                    <div class="editar">
-                                        <button class="bot-editar chamamodal" id_value="<?= $servico->id_servico ?>"
-                                            data-btn-modal="editar-servico">
-                                            <img id="icone-editar" src="../../public/img/icons/editar.png" alt="Editar">
-                                        </button>
-                                    </div>
-                                </td>
-                                <td class="inativar-menor">
-                                    <button class="switch_status toggle-btn <?= $classeStatus ?>"
-                                        id_value_switch="<?= $servico->id_servico ?>" data-btn-modal="status-servico">
-                                        <div class="circulo"></div>
-                                    </button>
-                                </td>
+            <div id="tela-branca-Ponto-atendimento">
+                <div class="tabela-responsiva-Ponto-atendimento">
+                    <table id="table table-striped" class="tabela-Ponto-atendimento">
+                        <thead class="cabecaTabelaPonto-atendimento">
+                            <tr class="topo-tabela-servicos">
+                                <th class="cabecalho-tabela1">Código</th>
+                                <th class="indentificador-menor" class="cabecalho-tabela2">Nome do Serviço</th>
+                                <th class="editar-menor" class="cabecalho-tabela3">Editar</th>
+                                <th class="inativar-menor" class="cabecalho-tabela1">Status</th>
                             </tr>
-                        <?php endforeach; ?>
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody class="resto-tabela-Ponto-atendimento">
+                            <?php foreach ($servicos_cadastrados as $servico): ?>
+                                <?php $classeStatus = ($servico->status_servico == 1) ? 'active' : ''; ?>
+                                <tr>
+                                    <td><?= $servico->nome_servico ?></td>
+                                    <td class="indentificador-menor"><?= $servico->codigo_servico ?></td>
+                                    <td class="editar-menor">
+                                        <div class="editar">
+                                            <button class="bot-editar chamamodal" id_value="<?= $servico->id_servico ?>"
+                                                data-btn-modal="editar-servico">
+                                                <img id="icone-editar" src="../../public/img/icons/editar.png" alt="Editar">
+                                            </button>
+                                        </div>
+                                    </td>
+                                    <td class="inativar-menor">
+                                        <button class="switch_status toggle-btn <?= $classeStatus ?>"
+                                            id_value_switch="<?= $servico->id_servico ?>" data-btn-modal="status-servico">
+                                            <div class="circulo"></div>
+                                        </button>
+                                    </td>
+                                </tr>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                </div>
             </div>
-        </div>
-        <!-- </div> <button id="" id_value_switch="1"  class="switch_status toggle-btn '.$estadoAtivo.'" data-btn-modal="status-servico"></button> -->
-        <div class="botoesVoltar-Cadastrar">
-            <button type="button" class="botao-voltar" onclick="window.location.href='menuadm_servicos.php';">Voltar</button>
-            <!-- <button type="button" id="abrirModal" class="botao-cadastro">Cadastrar</button> -->
-            <button type="button" id="btn_cadastrar_servico" class="botao-cadastro"
-                data-btn-modal="cadastrar">Cadastrar</button>
-        </div>
-</section>
-<?php
-include_once "./monitor_modal.php";
-include_once "../../public/modais/modal_cadastro_servico.php";
-include_once "../../public/modais/modal_edicao_servico.php";
-include_once "../../public/modais/modal_confirmacao_dados_registrados.php";
-include_once "../../public/modais/modal_confirmacao_dados.php";
-include_once "../../public/modais/modal_status_servico.php";
-include_once "../../public/modais/modal_aviso_erro.php";
-?>
+            <!-- </div> <button id="" id_value_switch="1"  class="switch_status toggle-btn '.$estadoAtivo.'" data-btn-modal="status-servico"></button> -->
+            <div class="botoesVoltar-Cadastrar">
+                <button type="button" class="botao-voltar" onclick="window.location.href='menuadm_servicos.php';">Voltar</button>
+                <!-- <button type="button" id="abrirModal" class="botao-cadastro">Cadastrar</button> -->
+                <button type="button" id="btn_cadastrar_servico" class="botao-cadastro"
+                    data-btn-modal="cadastrar">Cadastrar</button>
+            </div>
+    </section>
+    <?php
+    include_once "./monitor_modal.php";
+    include_once "../../public/modais/modal_cadastro_servico.php";
+    include_once "../../public/modais/modal_edicao_servico.php";
+    include_once "../../public/modais/modal_confirmacao_dados_registrados.php";
+    include_once "../../public/modais/modal_confirmacao_dados.php";
+    include_once "../../public/modais/modal_status_servico.php";
+    include_once "../../public/modais/modal_aviso_erro.php";
+    ?>
 </body>
 
 </html>
