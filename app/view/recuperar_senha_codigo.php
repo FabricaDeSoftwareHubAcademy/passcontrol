2b40fc34081d129f67fc0f3a459b5de037ad01bd
Juliana <juliana29.bmartins@gmail.com>
2025-07-25 08:38:32 -0400

    refactor: arrumando os css e corrigindo a pag de login

--- app/view/recuperar_senha_codigo.php
+++ app/view/recuperar_senha_codigo.php
@@ -26,7 +26,7 @@ if (isset($_POST['validar'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PassControl</title>
 
-    <link rel="stylesheet" href="../../public/css/root.css">
+    <!-- <link rel="stylesheet" href="../../public/css/root.css"> -->
     <link rel="stylesheet" href="../../public/css/forms.css">
     <link rel="shortcut icon" href="../../public/img/favicon.ico" type="image/x-icon">
 
@@ -40,18 +40,18 @@ if (isset($_POST['validar'])) {
                 <img src="../../public/img/logo_animation/animation_logo_mid.png" alt="imagem da animação meio" class="image_animation image_animation_mid">
                 <img src="../../public/img/logo_animation/animation_logo_bot.png" alt="imagem da animação baixo" class="image_animation image_animation_bot">
             </div>
-            <h1 class="title">PASS CONTROL</h1>
+            <h1 class="title-login">PASS CONTROL</h1>
         </div>
 
-        <form id="form_codigo" method="POST" class="form">
-            <div class="input_group">
+        <form id="form_codigo" method="POST" class="form-login">
+            <div class="input_group-login">
                 <label class="label_recuperar_senha" for="">Recuperar Senha</label>
                 <div class="container_input_codigo">
-                    <input type="text" class="input" name="codigo1" id="input_1" maxlength="1" onkeyup="mudaFoco(this, 1, input_2)">
-                    <input type="text" class="input" name="codigo2" id="input_2" maxlength="1" onkeyup="mudaFoco(this, 1, input_3)">
-                    <input type="text" class="input" name="codigo3" id="input_3" maxlength="1" onkeyup="mudaFoco(this, 1, input_4)">
-                    <input type="text" class="input" name="codigo4" id="input_4" maxlength="1" onkeyup="mudaFoco(this, 1, input_5)">
-                    <input type="text" class="input" name="codigo5" id="input_5" maxlength="1" onkeyup="mudaFoco(this, 1, null)">
+                    <input type="text" class="input-login" name="codigo1" id="input_1" maxlength="1" onkeyup="mudaFoco(this, 1, input_2)">
+                    <input type="text" class="input-login" name="codigo2" id="input_2" maxlength="1" onkeyup="mudaFoco(this, 1, input_3)">
+                    <input type="text" class="input-login" name="codigo3" id="input_3" maxlength="1" onkeyup="mudaFoco(this, 1, input_4)">
+                    <input type="text" class="input-login" name="codigo4" id="input_4" maxlength="1" onkeyup="mudaFoco(this, 1, input_5)">
+                    <input type="text" class="input-login" name="codigo5" id="input_5" maxlength="1" onkeyup="mudaFoco(this, 1, null)">
                 </div>
             </div>
 
@@ -59,8 +59,8 @@ if (isset($_POST['validar'])) {
 
             <p class="text_recuperar_senha">Insira o código recebido no e-mail.</p>
 
-            <div class="container_button">
-                <button type="submit" id="btn_enviar" name="validar" class="button">Enviar</button>
+            <div class="container_button-login">
+                <button type="submit" id="btn_enviar" name="validar" class="button-login">Enviar</button>
             </div>
         </form>
     </main>
