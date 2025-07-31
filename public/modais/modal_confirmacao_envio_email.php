469474b0343f85c69d20e8149ba5b51f7bfe0d5e
GabrAlvSM <gabriel.alvin7@gmail.com>
2025-07-31 11:50:55 -0400

    fix: corrigindo mensagem de login com senha incorreta e adicao de filtros em variaveis desprotegidas

--- public/modais/modal_confirmacao_envio_email.php
+++ public/modais/modal_confirmacao_envio_email.php
@@ -4,7 +4,7 @@
         <h1 class="modal-title-confirmacao-senha">Confirmação</h1>
         <hr class="modal-divider-confirmacao-senha">
         <img src="../../public/img/img-modais/tick-circlecheck.png" alt="Check" class="check-image-confirmacao-senha">
-        <p class="modal-message-confirmacao-senha"><b>Código enviado com sucesso!</b></p>
+        <p class="modal-message-confirmacao-senha">Código enviado com sucesso!</p>
         <div class="button-group-confirmacao-senha">
             <button class="botao-modal-confirmacao-senha Okay_ConfSenha">Ok</button>
         </div>
